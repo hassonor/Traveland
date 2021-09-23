64c0c0bf361810c2f6c1045188e2a3c8b04aa514
Or Hasson <hassonor@gmail.com>
2021-09-23 11:12:00 +0300

    refactor btn animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,17 +10,13 @@ const Header = ({heading, paragraph, children }) => {
             <div className="header__video">
             <video src={state.video} autoPlay loop muted poster={state.poster}/>
             </div>
-            <div className="header__contents">
-                <div className="container">
-                    <div className="header__contents__text">
-                        <div className="header__contents__text__child">
-                            <h1 className="header__contents__text__child__h1">
-                                {heading}
-                            </h1>
-                            <p className="header__contents__text__child__p">
-                                {paragraph}
-                            </p>
-                            <div className="header__contents__child__link">
+            <div className='header__contents'>
+                <div className='container'>
+                    <div className='header__contents__text'>
+                        <div className='header__contents__text__child'>
+                            <h1 className='header__contents__text__child__h1'>{heading}</h1>
+                            <p className='header__contents__text__child__p'>{paragraph}</p>
+                            <div className='header__contents__text__child__link'>
                                 {children}
                             </div>
                         </div>
