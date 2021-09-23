f7ac7d7adb401207c3bf415b0c1f6d907123dcfc
Or Hasson <hassonor@gmail.com>
2021-09-23 10:11:09 +0300

    Adding bg video, header and button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,31 @@
 import {useState} from "react";
 
-const Header = () => {
+const Header = ({heading, paragraph, children }) => {
     const [state] = useState({
-        video: './assets/videos/header.mp4'
+        video: './assets/videos/video.mp4',
+        poster: "/assets/images/screen.png"
     })
     return(
         <div className="header">
             <div className="header__video">
-            <video src={state.video} autoPlay loop muted></video>
+            <video src={state.video} autoPlay loop muted poster={state.poster}/>
+            </div>
+            <div className="header__contents">
+                <div className="container">
+                    <div className="header__contents__text">
+                        <div className="header__contents__text__child">
+                            <h1 className="header__contents__text__child__h1">
+                                {heading}
+                            </h1>
+                            <p className="header__contents__text__child__p">
+                                {paragraph}
+                            </p>
+                            <div className="header__contents__child__link">
+                                {children}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import Header from "../components/Header";
+import {useState} from 'react';
+
 
 const Home = () => {
+    const [state] = useState({
+        heading: 'Travel for your Soul.',
+        paragraph: 'Join us Today! Travel the most beautiful places in the world!'
+    })
     return (
         <>
-        <Header/>
+        <Header heading={state.heading} paragraph={state.paragraph}>
+            <button className="btn-default">Press Here</button>
+        </Header>
         </>
     )
 }
