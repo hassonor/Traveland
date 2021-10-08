891689704d24987adb48054a5297ca9ae823767d
Or Hasson <hassonor@gmail.com>
2021-10-08 18:13:00 +0300

    set 3 reviews on HomePage

--- src/data/reviews.js
+++ src/data/reviews.js
@@ -23,51 +23,51 @@ const reviews = [
 		stars: 3,
 		image: '/assets/reviewUsers/user3.jpg',
 	},
-	{
-		id: 4,
-		name: 'ראמזי',
-		comment: 'אני אוהב את המלונות שקשורים לiTravel.',
-		stars: 5,
-		image: '/assets/reviewUsers/user4.jpg',
-	},
-	{
-		id: 5,
-		name: 'שירה',
-		comment:
-			'אני ובעלי שנינו מעריצים גדולים של אתר של iTravel מכיוון שיש להם כל מה שאתה רוצה, חדרים בדירוג של חמישה כוכבים, מדריכי טיולים ידידותיים, חבילות זולות והנחות מיוחדות לזוגות, המילים האחרונות שלי אתם חייבים לעשות סיור אחד עם iTravel לאחר מכן גם אתם תאוהבו את iTravel.',
-		stars: 5,
-		image: '/assets/reviewUsers/user5.jpg',
-	},
-	{
-		id: 6,
-		name: 'ערן',
-		comment:
-			"אני אוהב את השירותים במיוחד כשהגעתי לארה\"ב, מכונית של iTravel הייתה שם בשבילי.",
-		stars: 2,
-		image: '/assets/reviewUsers/user6.jpg',
-	},
-	{
-		id: 7,
-		name: 'ניוטון',
-		comment: 'תודה לכם iTravel על מסע מופלא.',
-		stars: 5,
-		image: '/assets/reviewUsers/user7.jpg',
-	},
-	{
-		id: 8,
-		name: 'גיא',
-		comment:
-			'נסעתי גם עם כמה סוכנויות אחרות אבל iTravel מקצועיים יותר מאחרים.',
-		stars: 5,
-		image: '/assets/reviewUsers/user8.jpg',
-	},
-	{
-		id: 9,
-		name: 'סאמיר',
-		comment: 'מומלץ בחום לכולם! תסמכו עלי!',
-		stars: 5,
-		image: '/assets/reviewUsers/user9.jpg',
-	},
+	// {
+	// 	id: 4,
+	// 	name: 'ראמזי',
+	// 	comment: 'אני אוהב את המלונות שקשורים לiTravel.',
+	// 	stars: 5,
+	// 	image: '/assets/reviewUsers/user4.jpg',
+	// },
+	// {
+	// 	id: 5,
+	// 	name: 'שירה',
+	// 	comment:
+	// 		'אני ובעלי שנינו מעריצים גדולים של אתר של iTravel מכיוון שיש להם כל מה שאתה רוצה, חדרים בדירוג של חמישה כוכבים, מדריכי טיולים ידידותיים, חבילות זולות והנחות מיוחדות לזוגות, המילים האחרונות שלי אתם חייבים לעשות סיור אחד עם iTravel לאחר מכן גם אתם תאוהבו את iTravel.',
+	// 	stars: 5,
+	// 	image: '/assets/reviewUsers/user5.jpg',
+	// },
+	// {
+	// 	id: 6,
+	// 	name: 'ערן',
+	// 	comment:
+	// 		"אני אוהב את השירותים במיוחד כשהגעתי לארה\"ב, מכונית של iTravel הייתה שם בשבילי.",
+	// 	stars: 2,
+	// 	image: '/assets/reviewUsers/user6.jpg',
+	// },
+	// {
+	// 	id: 7,
+	// 	name: 'ניוטון',
+	// 	comment: 'תודה לכם iTravel על מסע מופלא.',
+	// 	stars: 5,
+	// 	image: '/assets/reviewUsers/user7.jpg',
+	// },
+	// {
+	// 	id: 8,
+	// 	name: 'גיא',
+	// 	comment:
+	// 		'נסעתי גם עם כמה סוכנויות אחרות אבל iTravel מקצועיים יותר מאחרים.',
+	// 	stars: 5,
+	// 	image: '/assets/reviewUsers/user8.jpg',
+	// },
+	// {
+	// 	id: 9,
+	// 	name: 'סאמיר',
+	// 	comment: 'מומלץ בחום לכולם! תסמכו עלי!',
+	// 	stars: 5,
+	// 	image: '/assets/reviewUsers/user9.jpg',
+	// },
 ];
 
 export default reviews;
